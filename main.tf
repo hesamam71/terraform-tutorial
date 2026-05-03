54369b3569d66ab2d913a9456abf6dad087913d1
Amirhesam Masrourfar <am25adn@herts.ac.uk>
2026-05-03 08:47:34 +0100

    v05

--- main.tf
+++ main.tf
@@ -1,47 +1,8 @@
-module "ec2_babak" {
-  source = "./module/ec2"
+resource "aws_instance" "herts1" {
+  ami           = "ami-0aa21e8848eea555b"
+  instance_type = "t3.micro"
 
-  instance_name = "v011"
-  key_name      = "dvh"
+  tags = {
+    Created_By = "terraform"
+  }
 }
-
-
-module "ec2_babak2" {
-  source = "./module/ec2"
-
-  instance_name = "v011_2"
-  key_name      = "dvh"
-}
-
-
-
-# resource "aws_instance" "babak" {
-#   ami           = var.ami_id
-#   instance_type = var.instance_type
-#   key_name      = var.key_name
-
-#   tags = {
-#     Name       = var.machine_name
-#     Created_By = "terraform"
-#   }
-# }
-# resource "aws_instance" "babak2" {
-#   ami = "ami-08cd358d745620807"
-
-#   instance_type = "t2.micro"
-
-#   tags = {
-#     Created_By = "terraform"
-#     Name       = "Airflow"
-#   }
-# }
-# terraform import aws_instance.babak3 i-01836e088c5f7f783
-# resource "aws_instance" "babak3" {
-#   ami = "ami-086b3de06dafe36c5"
-#   key_name      = var.key_name
-#   instance_type = "t2.micro"
-#    tags = {
-#         Name       = "babak2"
-#   }
-
-# }
--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "dvh-terraform-tutorial"
+    bucket = "hesam.herts.ac"
     key    = "build/airflow/terraform.tfstate"
-    region = "eu-west-2"
+    region = "eu-west-1"
   }
 
   required_providers {
@@ -11,8 +11,4 @@ terraform {
       version = "~> 4.0"
     }
   }
-}
-# Configure the AWS Provider
-provider "aws" {
-  region = var.region
 }
